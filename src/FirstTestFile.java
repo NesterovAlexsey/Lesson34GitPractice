7848eb485ac792cb13a60a81573f0777cb7ed7ff
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-06 17:48:44 +0100

    Close work with testing file

--- src/FirstTestFile.java
+++ src/FirstTestFile.java
@@ -1,6 +1,6 @@
 public class FirstTestFile {
   public static void main(String[] args) {
-    int a = 0;
+
     System.out.println("Parameter 'a' is never used, but it is not any mistake in IJ");
   }
 }
